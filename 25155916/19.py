95ac8f7c2fbcd91072e9f8903ca15e5613fac88b
AndIGo99 <159000973+AndIGo99@users.noreply.github.com>
2026-03-17 18:26:28 +0300

    Add files via upload

---
+++ 25155916/19.py
@@ -0,0 +1,15 @@
+def f(s,i): #s - кол-во камней на начало хода, i - кол-во оставшихся ходов
+    if i == 0: # последний ход
+        if s<5110 and s*2 >= 5110: #прошлый игрок не победил, а текущий может
+            return True 
+        else:
+            return False
+    else:
+        if f(s+1,i-1) and  f(s+4,i-1) and f(s*2,i-1):
+            return True
+        else:
+            return False
+for s in range(1,5110):
+    if f(s,1):
+        print(s)
+    
